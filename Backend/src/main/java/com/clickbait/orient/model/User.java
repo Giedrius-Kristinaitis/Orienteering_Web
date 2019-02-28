5439485174bfe8879fa45f023ad66b4354031fef
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-02-28 19:26:28 +0200

    Created user model

---
+++ Backend/src/main/java/com/clickbait/orient/model/User.java
@@ -0,0 +1,59 @@
+package com.clickbait.orient.model;
+
+import org.hibernate.validator.constraints.Length;
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * User model class
+ */
+@SuppressWarnings("unused")
+public class User {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Length(min = 6)
+    private String email;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    @NotBlank
+    @Length(min = 5)
+    private String password;
+
+    /**
+     * Default no-args constructor
+     */
+    public User() { }
+
+    /**
+     * Constructor with arguments
+     */
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    // GETTERS
+    public String getId() { return id; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+
+    // SETTERS
+    public void setId(String id) { this.id = id; }
+    public void setEmail(String email) { this.email = email; }
+    public void setPassword(String password) { this.password = password; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+}
