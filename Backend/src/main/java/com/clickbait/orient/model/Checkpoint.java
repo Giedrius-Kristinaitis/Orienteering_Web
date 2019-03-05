3f3a94c647cfd51a63c1b295541319dc431a8972
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-03-05 21:16:56 +0200

    Created checkpoint model

---
+++ Backend/src/main/java/com/clickbait/orient/model/Checkpoint.java
@@ -0,0 +1,59 @@
+package com.clickbait.orient.model;
+
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.NotBlank;
+import java.math.BigDecimal;
+
+/**
+ * Checkpoint representation
+ */
+@SuppressWarnings("unused")
+public class Checkpoint {
+
+    @Id
+    private String id;
+
+    @DecimalMin("-90")
+    @DecimalMax("90")
+    private BigDecimal latitude;
+
+    @DecimalMin("-180")
+    @DecimalMax("180")
+    private BigDecimal longitude;
+
+    @NotBlank
+    private String name;
+
+    /**
+     * Default no-args constructor
+     */
+    public Checkpoint() {}
+
+    /**
+     * Constructor with arguments
+     * @param name
+     * @param latitude
+     * @param longitude
+     */
+    public Checkpoint(String id, String name, BigDecimal latitude, BigDecimal longitude) {
+        this.id = id;
+        this.name = name;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // GETTERS
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public BigDecimal getLatitude() { return latitude; }
+    public BigDecimal getLongitude() { return longitude; }
+
+    // SETTERS
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setLatitude(BigDecimal latitude) { this.latitude = latitude; }
+    public void setLongitude(BigDecimal longitude) { this.longitude = longitude; }
+}
