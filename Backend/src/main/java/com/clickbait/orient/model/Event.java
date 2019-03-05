4ae7b842a21f9bceff96abf3c5e2f00823dbcc38
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-03-05 21:51:10 +0200

    Finished event model

--- Backend/src/main/java/com/clickbait/orient/model/Event.java
+++ Backend/src/main/java/com/clickbait/orient/model/Event.java
@@ -1,4 +1,75 @@
 package com.clickbait.orient.model;
 
+import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+/**
+ * Event representation
+ */
+@SuppressWarnings("unused")
 public class Event {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    private String name;
+
+    @Min(1)
+    @NotNull
+    private Integer checkpointCount;
+
+    @NotNull
+    private List<Checkpoint> checkpoints;
+
+    @Min(1)
+    @NotNull
+    private Integer teamSize;
+
+    @NotNull
+    private List<Team> teams;
+
+    /**
+     * Default no-args constructor
+     */
+    public Event() {}
+
+    /**
+     * Constructor with arguments
+     *
+     * @param id
+     * @param name
+     * @param checkpointCount
+     * @param checkpoints
+     * @param teamSize
+     * @param teams
+     */
+    public Event(String id, String name, Integer checkpointCount, List<Checkpoint> checkpoints, Integer teamSize, List<Team> teams) {
+        this.id = id;
+        this.name = name;
+        this.checkpointCount = checkpointCount;
+        this.checkpoints = checkpoints;
+        this.teamSize = teamSize;
+        this.teams = teams;
+    }
+
+    // GETTERS
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public Integer getCheckpointCount() { return checkpointCount; }
+    public List<Checkpoint> getCheckpoints() { return checkpoints; }
+    public Integer getTeamSize() { return teamSize; }
+    public List<Team> getTeams() { return teams; }
+
+    // SETTERS
+    public void setId(String id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+    public void setCheckpointCount(Integer checkpointCount) { this.checkpointCount = checkpointCount; }
+    public void setCheckpoints(List<Checkpoint> checkpoints) { this.checkpoints = checkpoints; }
+    public void setTeamSize(Integer teamSize) { this.teamSize = teamSize; }
+    public void setTeams(List<Team> teams) { this.teams = teams; }
 }
