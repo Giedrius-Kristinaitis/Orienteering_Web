020b821473a825a75161c7d85df27f21d98be841
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-03-08 10:56:59 +0200

    Added event screen module

---
+++ Frontend/src/app/components/event-screen/event-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-screen',
+  templateUrl: './event-screen.component.html',
+  styleUrls: ['./event-screen.component.css']
+})
+export class EventScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
