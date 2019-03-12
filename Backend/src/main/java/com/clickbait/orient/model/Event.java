a1a03a345035ec54d9ddc667b62c5694e7d47d2c
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-03-12 20:38:14 +0200

    Added description and starting date fields to event model

--- Backend/src/main/java/com/clickbait/orient/model/Event.java
+++ Backend/src/main/java/com/clickbait/orient/model/Event.java
@@ -20,6 +20,9 @@ public class Event {
     @NotBlank
     private String name;
 
+    @NotBlank
+    private String description;
+
     @Min(1)
     @NotNull
     private Integer checkpointCount;
@@ -37,6 +40,9 @@ public class Event {
     @NotNull
     private Date created;
 
+    @NotNull
+    private Date starting;
+
     private EventStatus status;
 
     /**
@@ -44,44 +50,40 @@ public class Event {
      */
     public Event() {}
 
-    /**
-     * Constructor with arguments
-     *
-     * @param id
-     * @param name
-     * @param checkpointCount
-     * @param checkpoints
-     * @param teamSize
-     * @param teams
-     */
-    public Event(String id, String name, Integer checkpointCount, List<Checkpoint> checkpoints, Integer teamSize, List<Team> teams, Date created, EventStatus status) {
+    public Event(String id, String name, String description, Integer checkpointCount, List<Checkpoint> checkpoints, Integer teamSize, List<Team> teams, Date created, Date starting, EventStatus status) {
         this.id = id;
         this.name = name;
+        this.description = description;
         this.checkpointCount = checkpointCount;
         this.checkpoints = checkpoints;
         this.teamSize = teamSize;
         this.teams = teams;
         this.created = created;
+        this.starting = starting;
         this.status = status;
     }
 
     // GETTERS
     public String getId() { return id; }
     public String getName() { return name; }
+    public String getDescription() { return description; }
     public Integer getCheckpointCount() { return checkpointCount; }
     public List<Checkpoint> getCheckpoints() { return checkpoints; }
     public Integer getTeamSize() { return teamSize; }
     public List<Team> getTeams() { return teams; }
     public Date getCreated() { return created; }
+    public Date getStarting() { return starting; }
     public EventStatus getStatus() { return status; }
 
     // SETTERS
     public void setId(String id) { this.id = id; }
     public void setName(String name) { this.name = name; }
+    public void setDescription(String description) { this.description = description; }
     public void setCheckpointCount(Integer checkpointCount) { this.checkpointCount = checkpointCount; }
     public void setCheckpoints(List<Checkpoint> checkpoints) { this.checkpoints = checkpoints; }
     public void setTeamSize(Integer teamSize) { this.teamSize = teamSize; }
     public void setTeams(List<Team> teams) { this.teams = teams; }
     public void setCreated(Date created) { this.created = created; }
+    public void setStarting(Date starting) { this.starting = starting; }
     public void setStatus(EventStatus status) { this.status = status; }
 }
