24c0c3ad28ddf9ab0f8dd390b61ec6f4122b4d46
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-03-29 08:40:28 +0200

    Added routing to create event page

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {EventScreenComponent} from "./components/event-screen/event-screen.component";
 import {EventDetailScreenComponent} from "./components/event-detail-screen/event-detail-screen.component";
+import {EventCreateScreenComponent} from "./components/event-create-screen/event-create-screen.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/events', pathMatch: 'full' },
   { path: 'events', component: EventScreenComponent },
-  { path: 'event/detail/:id', component: EventDetailScreenComponent }
+  { path: 'event/detail/:id', component: EventDetailScreenComponent },
+  { path: 'event/create', component: EventCreateScreenComponent }
 ];
 
 @NgModule({
