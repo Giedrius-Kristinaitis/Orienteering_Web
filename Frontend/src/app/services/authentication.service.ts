663531226b28f763d42d13614065d474fb69a49e
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-04-02 11:04:14 +0300

    Changed login method so you could check if login was successful

--- Frontend/src/app/services/authentication.service.ts
+++ Frontend/src/app/services/authentication.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {User} from "../components/user";
 import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable, Subject, Subscription} from "rxjs";
+import {map} from "rxjs/operators";
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -16,14 +18,11 @@ export class AuthenticationService {
 
   constructor(private http: HttpClient) { }
 
-  login(email: string, password: string): boolean {
-    this.http.post<User>('http://localhost:8080/user/login', {email: email, password: password}, httpOptions).subscribe(user => {
+  login(email: string, password: string) {
+    return this.http.post<User>('http://localhost:8080/user/login', {email: email, password: password}, httpOptions).pipe(map(user => {
       if(user) {
         localStorage.setItem('currentUser', JSON.stringify(user));
-
-        return true;
       }
-    });
-    return false;
+    }));
   }
 }
