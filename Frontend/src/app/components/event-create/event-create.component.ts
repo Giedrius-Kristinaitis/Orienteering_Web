390fefb99b87699bd927235097e92b0660a5bfcc
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-04-28 13:09:18 +0300

    Added form to create event

--- Frontend/src/app/components/event-create/event-create.component.ts
+++ Frontend/src/app/components/event-create/event-create.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import * as moment from 'moment';
+import {EventService} from '../../services/event.service';
+import {MessageService} from '../../services/message.service';
+import {Router} from '@angular/router';
+import {Marker} from '../marker';
 
 @Component({
   selector: 'app-event-create',
@@ -7,38 +12,141 @@ import {FormControl, Validators} from "@angular/forms";
   styleUrls: ['./event-create.component.css']
 })
 export class EventCreateComponent implements OnInit {
-  name = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(16)]);
-  teamSize = new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'), Validators.min(1), Validators.max(30)]);
-  date = new FormControl('', [Validators.required, Validators.min(new Date().getMilliseconds()), Validators.pattern('^([0-2][0-9]|(3)[0-1])(\\/)(((0)[0-9])|((1)[0-2]))(\\/)\\d{4}$')]);
+  readonly minTeamLimit = 1;
+  readonly maxTeamLimit = 100;
+  readonly minNameLength = 4;
+  readonly maxNameLength = 16;
+  readonly minDistance = 1;
+  readonly maxDistance = 1_000_000;
+  readonly minDuration = 1;
+  readonly maxDuration = 1_000_000;
+  readonly minDescriptionLength = 1;
+  readonly maxDescriptionLength = 200;
+  readonly minCheckpoints = 1;
+  readonly maxCheckpoints = 100;
 
-  dateInput = new Date();
+  eventForm: FormGroup;
+  submitted = false;
 
-  constructor() {
+  minDate: Date;
+  maxDate: Date;
+
+  checkpointsCount = 0;
+  checkpointsList: Marker[] = [];
+
+  constructor(private eventService: EventService,
+              private messageService: MessageService,
+              private router: Router) {
+  }
+
+  ngOnInit() {
+    // // Date limits for data picker input
+    this.minDate = new Date(Date.now());
+    this.maxDate = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
+
+    this.eventForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(this.minNameLength),
+        Validators.maxLength(this.maxNameLength)]),
+      teamSize: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'),
+        Validators.min(this.minTeamLimit), Validators.max(this.maxTeamLimit)]),
+      date: new FormControl('', [Validators.required, Validators.min(new Date().getMilliseconds())]),
+      time: new FormControl('', [Validators.required, Validators.pattern('^(0[0-9]|1[0-9]|2[0-3]|[0-9]):[0-5][0-9]$')]),
+      duration: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'),
+        Validators.min(this.minDuration), Validators.max(this.maxDuration)]),
+      distance: new FormControl('', [Validators.required, Validators.pattern('^[0-9]+$'),
+        Validators.min(this.minDistance), Validators.max(this.maxDistance)]),
+      description: new FormControl('', [Validators.required,
+        Validators.minLength(this.minDescriptionLength), Validators.maxLength(this.maxDescriptionLength)]),
+      checkpointsCount: new FormControl(this.checkpointsCount, [Validators.min(this.minCheckpoints),
+        Validators.max(this.maxCheckpoints)])
+    });
   }
 
   getErrorMessage(form: FormControl) {
     switch (form) {
-      case this.name: {
+      case this.eventForm.get('name'): {
         return form.hasError('required') ? 'You must enter a name' :
-          form.hasError('minlength') ? 'Must be at least 4 characters' :
-            form.hasError('maxlength') ? 'Must not exceed 16 characters' : '';
+          form.hasError('minlength') ? 'Must be at least ' + this.minNameLength + ' characters' :
+            form.hasError('maxlength') ? 'Must not exceed ' + this.maxNameLength + ' characters' : '';
       }
-      case this.teamSize: {
+      case this.eventForm.get('teamSize'): {
         return form.hasError('required') ? 'You must enter a team size' :
           form.hasError('pattern') ? 'You must enter a number' :
-            form.hasError('min') ? 'You must enter value higher or equal to 0' :
-              form.hasError('max') ? 'You must enter a value lower or equal to 30' : '';
+            form.hasError('min') ? 'You must enter value higher than 0' :
+              form.hasError('max') ? 'You must enter a value lower or equal to ' + this.maxTeamLimit : '';
       }
-      case this.date: {
+      case this.eventForm.get('date'): {
+        // return console.log(form);
         return form.hasError('required') ? 'You must select a date' :
-          form.hasError('min') ? 'You can not select previous date' :
-            form.hasError('pattern') ? 'You must enter a date' : '';
+          form.hasError('matDatepickerMin') ? 'You  can not select previous date' :
+            form.hasError('matDatepickerMax') ? 'You must select date within a year' : '';
+      }
+      case this.eventForm.get('time'): {
+        return form.hasError('required') ? 'You must select event starting time' :
+          form.hasError('pattern') ? 'You must enter time in correct format' : '';
+      }
+      case this.eventForm.get('duration'): {
+        return form.hasError('pattern') ? 'You must enter a valid number' :
+          form.hasError('min') ? 'Number must be equal or higher than ' + this.minDuration :
+            form.hasError('max') ? 'Number must be equal or lower than ' + this.maxDuration :
+              form.hasError('required') ? 'You must enter a number' : '';
+      }
+      case this.eventForm.get('distance'): {
+        return form.hasError('pattern') ? 'You must enter a valid numbr' :
+          form.hasError('min') ? 'Number must be equal or higher than ' + this.minDistance :
+            form.hasError('max') ? 'Number must be equal or lower than ' + this.maxDistance :
+              form.hasError('required') ? 'You must enter a number' : '';
+      }
+      case this.eventForm.get('description'): {
+        return form.hasError('required') ? 'You must write something' :
+          form.hasError('minLength') ? 'Text length must be equal or highter than ' + this.minDescriptionLength :
+            form.hasError('maxLength') ? 'Text length must be equal or lower than' + this.maxDescriptionLength : '';
+      }
+      case this.eventForm.get('checkpointsCount') : {
+        return form.hasError('min') ? 'You must at least add ' + this.minCheckpoints +
+          (this.minCheckpoints === 1 ? ' checkpoint' : 'checkpoints') :
+          form.hasError('max') ? 'Checkpoints count must be equal or lower than ' + this.maxCheckpoints : '';
       }
     }
   }
 
-  ngOnInit() {
+  onSubmit(): void {
+    this.submitted = true;
+    this.eventForm.get('checkpointsCount').updateValueAndValidity();
+    console.log(this.eventForm.get('checkpointsCount').value)
 
+    if (this.eventForm.valid) {
+      const time = moment(this.eventForm.get('time').value, 'HH:mm');
+      const startingDate = time.hour() * 3_600_000 + time.minutes() * 60_000 + this.eventForm.get('date').value.valueOf();
+      const estimatedDuration = this.eventForm.get('duration').value * 3_600_000;
+
+      const newEvent = {
+        name: this.eventForm.get('name').value,
+        teamSize: this.eventForm.get('teamSize').value,
+        starting: new Date(startingDate).toJSON(),
+        estimatedTimeMillis: estimatedDuration,
+        estimatedDistanceMetres: this.eventForm.get('distance').value,
+        description: this.eventForm.get('description').value,
+        checkpointCount: this.checkpointsCount,
+        checkpoints: this.checkpointsList
+      };
+
+      this.eventService.addEvent(JSON.stringify(newEvent)).subscribe(
+        date => {
+        },
+        error => this.messageService.add(error.toString()),
+        () => {
+          this.router.navigateByUrl('/events');
+        }
+      );
+      this.messageService.clear();
+    }
   }
 
+  getCheckpoints(list: Marker[]): void {
+    this.checkpointsList = list;
+    this.checkpointsCount = list.length;
+    this.eventForm.get('checkpointsCount').setValue(this.checkpointsCount);
+    this.eventForm.get('checkpointsCount').updateValueAndValidity();
+  }
 }
