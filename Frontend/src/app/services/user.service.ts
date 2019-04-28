ca9988bf5e1d5ce628e01d3e2c9b93b03cc5196a
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-04-28 13:16:19 +0300

    Changed double quotes to single

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
-import {User} from "../components/user";
-import {Router} from "@angular/router";
+import {User} from '../components/user';
+import {Router} from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
