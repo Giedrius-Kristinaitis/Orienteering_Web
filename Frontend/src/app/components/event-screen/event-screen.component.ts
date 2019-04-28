d80b94546ca346f3abbc6790d850b124fe6d8fa9
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-04-28 13:12:22 +0300

    Added message component for displaying errors to user

--- Frontend/src/app/components/event-screen/event-screen.component.ts
+++ Frontend/src/app/components/event-screen/event-screen.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-event-screen',
@@ -7,7 +7,7 @@ import {Router} from "@angular/router";
   styleUrls: ['./event-screen.component.css']
 })
 export class EventScreenComponent implements OnInit {
-  eventName: string = '';
+  eventName = '';
 
   constructor(private router: Router) {
   }
