9b778807594848b7af6fd5b5607fb827f57ed7dd
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-05-09 11:07:17 +0300

    Added event constructor function

--- Frontend/src/app/components/event.ts
+++ Frontend/src/app/components/event.ts
@@ -14,4 +14,23 @@ export class Event {
   status: string;
   estimatedTimeMillis: number;
   estimatedDistanceMetres: number;
+
+  constructor(event: Event) {
+    if (event === null) {
+      return;
+    }
+
+    this.id = event.id;
+    this.name = event.name;
+    this.description = event.description;
+    this.checkpointCount = event.checkpointCount;
+    this.checkpoints = event.checkpoints;
+    this.teamSize = event.teamSize;
+    this.teams = event.teams;
+    this.created = event.created;
+    this.starting = event.starting;
+    this.status = event.status;
+    this.estimatedTimeMillis = event.estimatedTimeMillis;
+    this.estimatedDistanceMetres = event.estimatedDistanceMetres;
+  }
 }
