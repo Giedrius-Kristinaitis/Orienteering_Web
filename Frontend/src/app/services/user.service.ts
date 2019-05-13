2f72318217e8aeccd2d78376e4918ef0be123b7b
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-05-13 20:27:40 +0300

    Added error handling method, add user method

--- Frontend/src/app/services/user.service.ts
+++ Frontend/src/app/services/user.service.ts
@@ -1,13 +1,25 @@
 import {Injectable} from '@angular/core';
 import {User} from '../components/user';
 import {Router} from '@angular/router';
+import {EventResponse} from '../components/eventResponse';
+import {catchError, retry} from 'rxjs/operators';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {throwError} from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  })
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private static readonly host = 'http://104.196.227.120';
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+              private http: HttpClient) {
   }
 
   getUser(id: number): User {
@@ -21,5 +33,55 @@ export class UserService {
   logout(): void {
     localStorage.removeItem('currentUser');
     this.router.navigate(['login']);
+  }
+
+  addUser(user: User) {
+    return this.http.post<User>(`${UserService.host}`, user, httpOptions).pipe(
+      retry(1),
+      catchError(this.handleError)
+    );
+  }
+
+  /**
+   * Handles requests errors
+   * @param error Error
+   */
+  private handleError(error) {
+    let errorMessage = '';
+    if (error.error instanceof ErrorEvent) {
+      // client-side error
+      errorMessage = `Error: ${error.error.message}`;
+    } else {
+      // server-side error
+      errorMessage = `Error: ${error.status}`;
+    }
+
+    // window.alert(errorMessage);
+    return throwError(this.selectErrorMessage(errorMessage));
+  }
+
+  /**
+   * Formats error codes to messages
+   * @param status Request status
+   */
+  selectErrorMessage(status: string) {
+    let message = status;
+
+    switch (status) {
+      case '404' : {
+        message += ' Event not found';
+        break;
+      }
+      case '400' : {
+        message += ' Bad request';
+        break;
+      }
+      case '409' : {
+        message += ' Event already exists';
+        break;
+      }
+    }
+
+    return message;
   }
 }
