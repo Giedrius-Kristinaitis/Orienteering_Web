110b92cb039f9afba87315b40fd204fa10f75ab9
Giedrius Kristinaitis <kristinaitis.giedrius@gmail.com>
2019-05-14 21:18:57 +0300

    Added sign-up endpoint

--- Backend/src/main/java/com/clickbait/orient/controller/UserController.java
+++ Backend/src/main/java/com/clickbait/orient/controller/UserController.java
@@ -3,9 +3,11 @@ package com.clickbait.orient.controller;
 import com.clickbait.orient.dto.UserDTO;
 import com.clickbait.orient.model.User;
 import com.clickbait.orient.service.UserService;
+import com.clickbait.orient.validation.OnRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +20,7 @@ import javax.validation.Valid;
  */
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
 
     // handles user actions
@@ -45,5 +48,23 @@ public class UserController {
         }
 
         return new ResponseEntity<>(userDto, HttpStatus.OK);
+    }
+
+    /**
+     * Registers a new user
+     *
+     * @param user user to register
+     * @return registered user
+     */
+    @PostMapping("/sign-up")
+    @Validated(OnRegistration.class)
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody User user) {
+        UserDTO registered = service.registerUser(user);
+
+        if (registered == null) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+
+        return new ResponseEntity<>(registered, HttpStatus.OK);
     }
 }
