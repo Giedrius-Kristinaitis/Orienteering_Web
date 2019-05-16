5b4abad49460d94daf85077a8c799d44feb38ae3
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-05-16 09:11:15 +0300

    Added team join dialog

--- Frontend/src/app/components/event-join/event-join.component.ts
+++ Frontend/src/app/components/event-join/event-join.component.ts
@@ -2,6 +2,10 @@ import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
 import {MarkerEditDialogComponent} from '../marker-edit-dialog/marker-edit-dialog.component';
 import {Event} from '../event';
+import {Team} from '../team';
+import {EventService} from '../../services/event.service';
+import {UserService} from '../../services/user.service';
+import {User} from '../user';
 
 @Component({
   selector: 'app-event-join',
@@ -11,21 +15,49 @@ import {Event} from '../event';
 export class EventJoinComponent implements OnInit {
 
   constructor(public dialogRef: MatDialogRef<MarkerEditDialogComponent>,
-              @Inject(MAT_DIALOG_DATA) public data: Event) { }
-
-  ngOnInit() {
+              @Inject(MAT_DIALOG_DATA) public data: Event,
+              private eventService: EventService,
+              private userService: UserService) {
   }
 
-  onJoinClick(): void {
-    // if (this.markerName.valid) {
-    //   this.dialogRef.close(this.data);
-    // }
-    this.dialogRef.close(true);
+  ngOnInit() {
+    console.log(this.data);
+  }
+
+  onJoinClick(team: Team): void {
+    console.log('Metode: ' + this.userService.getCurrentUser());
+    this.eventService.addTeamMember(this.data.id, team.id, this.userService.getCurrentUser()).subscribe(
+      data => {
+      },
+      error => {
+      },
+      () => {
+        team.members.push(this.userService.getCurrentUser());
+      }
+    );
   }
 
   onCancelClick(): void {
     this.dialogRef.close(false);
     // this.dialogRef.close(true);
+  }
+
+  onCreateClick(): void {
+
+  }
+
+  getUserTeam(): boolean {
+    let foundTeam = undefined;
+    const currentUser = this.userService.getCurrentUser();
+    this.data.teams.forEach(team => {
+      team.members.forEach( x => {
+        if (x.id === currentUser.id) {
+          foundTeam = team;
+        }
+      });
+    });
+
+    return foundTeam;
   }
 
 }
