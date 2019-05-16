8c59ac2b11f52e00debb1225f6b7eefa30bd6907
Justas-Milisiunas <justas.milisiunas@gmail.com>
2019-05-16 10:42:44 +0300

    Added team join, leave, create functions

--- Frontend/src/app/components/event-join/event-join.component.ts
+++ Frontend/src/app/components/event-join/event-join.component.ts
@@ -1,11 +1,13 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material';
+import {MAT_DIALOG_DATA, MatDialog, MatDialogRef} from '@angular/material';
 import {MarkerEditDialogComponent} from '../marker-edit-dialog/marker-edit-dialog.component';
 import {Event} from '../event';
 import {Team} from '../team';
 import {EventService} from '../../services/event.service';
 import {UserService} from '../../services/user.service';
 import {User} from '../user';
+import {MessageService} from '../../services/message.service';
+import {EventTeamCreateDialogComponent} from '../event-team-create-dialog/event-team-create-dialog.component';
 
 @Component({
   selector: 'app-event-join',
@@ -17,19 +19,27 @@ export class EventJoinComponent implements OnInit {
   constructor(public dialogRef: MatDialogRef<MarkerEditDialogComponent>,
               @Inject(MAT_DIALOG_DATA) public data: Event,
               private eventService: EventService,
-              private userService: UserService) {
+              private userService: UserService,
+              private messageService: MessageService,
+              private dialog: MatDialog) {
   }
 
   ngOnInit() {
-    console.log(this.data);
+    // console.log(this.data);
   }
 
+  /**
+   * Adds user to selected team
+   * @param team Selected team
+   */
   onJoinClick(team: Team): void {
-    console.log('Metode: ' + this.userService.getCurrentUser());
+    // console.log('Metode: ' + this.userService.getCurrentUser());
     this.eventService.addTeamMember(this.data.id, team.id, this.userService.getCurrentUser()).subscribe(
       data => {
       },
       error => {
+        this.messageService.clear();
+        this.messageService.add(error);
       },
       () => {
         team.members.push(this.userService.getCurrentUser());
@@ -37,20 +47,51 @@ export class EventJoinComponent implements OnInit {
     );
   }
 
+  /**
+   * Removes user from selected team
+   * @param team Selected team
+   */
+  onLeaveClick(team: Team): void {
+    this.eventService.removeTeamMember(this.data.id, team.id, this.userService.getCurrentUser().id).subscribe(
+      data => {},
+      error => {
+        this.messageService.clear();
+        this.messageService.add(error);
+      },
+      () => {
+        team.members = team.members.filter(member => member.id !== this.userService.getCurrentUser().id);
+      }
+    );
+  }
+
+  /**
+   * Hides dialog after cancel button press
+   */
   onCancelClick(): void {
     this.dialogRef.close(false);
     // this.dialogRef.close(true);
   }
 
+  /**
+   * Opens create team dialog
+   */
   onCreateClick(): void {
+    const dialogRef = this.dialog.open(EventTeamCreateDialogComponent, {
+      data: this.data
+    });
 
+    dialogRef.afterClosed().subscribe();
   }
 
-  getUserTeam(): boolean {
-    let foundTeam = undefined;
+  /**
+   * Gets current user joined team object
+   */
+  getUserTeam(): Team {
+    let foundTeam;
     const currentUser = this.userService.getCurrentUser();
+
     this.data.teams.forEach(team => {
-      team.members.forEach( x => {
+      team.members.forEach(x => {
         if (x.id === currentUser.id) {
           foundTeam = team;
         }
